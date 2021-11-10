ccea0d12903f01233d8fe97c33b6f5117bc1aa07
Kimmo Lehto <klehto@mirantis.com>
2021-11-10 16:03:13 +0200

    More tuning

--- Formula/k0sctl.rb
+++ Formula/k0sctl.rb
@@ -1,12 +1,9 @@
 class K0sctl < Formula
   desc "Bootstrapping and management tool for k0s kubernetes clusters"
   homepage "https://github.com/k0sproject/k0sctl"
-  version "0.11.4"
   url "https://github.com/k0sproject/k0sctl.git", 
-    tag: "v#{version}", 
+    tag: "v0.11.4", 
     revision: "3b2e58b0915c92a972995d3d5d738e81a00fce96"
-
-  sha256 ""
   license "Apache-2"
   head "https://github.com/k0sproject/k0sctl.git", branch: "main"
 
@@ -18,7 +15,7 @@ class K0sctl < Formula
   end
 
   test do
-    assert_match "version: #{version}", shell_output("#{bin}/k0sctl", "version")
+    assert_match "version: v#{version}", shell_output("#{bin}/k0sctl", "version")
     asset_match "address: 10.0.0.1", shell_output("#{bin}/k0sctl", "init", "root@10.0.0.1")
   end
 end
