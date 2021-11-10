80b1e5cbea0f8dc17572c1d693485ad7237ae680
Kimmo Lehto <kimmo.lehto@gmail.com>
2021-11-10 15:27:36 +0200

    Initial k0sctl formula
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

---
+++ Formula/k0sctl.rb
@@ -0,0 +1,19 @@
+class K0sctl < Formula
+  desc "A bootstrapping and management tool for k0s kubernetes clusters"
+  homepage "https://github.com/k0sproject/k0sctl"
+  version "0.11.3"
+  url "https://github.com/k0sproject/k0sctl.git", using: :git, tag: "v#{version}"
+  sha1 ""
+  license "Apache-2"
+
+  depends_on "go@1.17" => :build
+
+  def install
+    system "make", "install", "PREFIX=#{prefix}"
+  end
+
+  test do
+    assert_match "version: #{version}", shell_output bin/"k0sctl", "version"
+    asset_match "address: 10.0.0.1", shell_output bin/"k0sctl", "init", "root@10.0.0.1"
+  end
+end
