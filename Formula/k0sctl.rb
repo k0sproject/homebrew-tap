8a4b7471ca05de8ce11b06e85f229c0a92a47a5f
Kimmo Lehto <klehto@mirantis.com>
2021-11-10 15:46:27 +0200

    Syntax error

--- Formula/k0sctl.rb
+++ Formula/k0sctl.rb
@@ -1,7 +1,7 @@
 class K0sctl < Formula
   desc "A bootstrapping and management tool for k0s kubernetes clusters"
   homepage "https://github.com/k0sproject/k0sctl"
-  version "0.11.3"
+  version "0.11.4"
   url "https://github.com/k0sproject/k0sctl.git", using: :git, tag: "v#{version}"
   sha1 ""
   license "Apache-2"
@@ -13,7 +13,7 @@ class K0sctl < Formula
   end
 
   test do
-    assert_match "version: #{version}", shell_output bin/"k0sctl", "version"
-    asset_match "address: 10.0.0.1", shell_output bin/"k0sctl", "init", "root@10.0.0.1"
+    assert_match "version: #{version}", shell_output "#{bin}/k0sctl", "version"
+    asset_match "address: 10.0.0.1", shell_output "#{bin}/k0sctl", "init", "root@10.0.0.1"
   end
 end
