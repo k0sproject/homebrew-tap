676253bd6d58c545d23666b614f2dde87b95754a
Kimmo Lehto <klehto@mirantis.com>
2021-11-10 15:59:02 +0200

    Finer headers

--- Formula/k0sctl.rb
+++ Formula/k0sctl.rb
@@ -2,9 +2,13 @@ class K0sctl < Formula
   desc "Bootstrapping and management tool for k0s kubernetes clusters"
   homepage "https://github.com/k0sproject/k0sctl"
   version "0.11.4"
-  url "https://github.com/k0sproject/k0sctl.git", using: :git, tag: "v#{version}"
+  url "https://github.com/k0sproject/k0sctl.git", 
+    tag: "v#{version}", 
+    revision: "3b2e58b0915c92a972995d3d5d738e81a00fce96"
+
   sha256 ""
   license "Apache-2"
+  head "https://github.com/k0sproject/k0sctl.git", branch: "main"
 
   depends_on "go@1.17" => :build
 
