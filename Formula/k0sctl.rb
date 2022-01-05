55c4a0a7cb48fd336897e348cccbc1d4f6a72ebc
Kimmo Lehto <klehto@mirantis.com>
2022-01-05 12:19:16 +0200

    k0sctl 0.12.1

--- Formula/k0sctl.rb
+++ Formula/k0sctl.rb
@@ -2,16 +2,10 @@ class K0sctl < Formula
   desc "Bootstrapping and management tool for k0s kubernetes clusters"
   homepage "https://github.com/k0sproject/k0sctl"
   url "https://github.com/k0sproject/k0sctl.git",
-      tag:      "v0.12.0",
-      revision: "610682e3ad9bc633cb31135fe4da8d43bf6f07d8"
+      tag:      "v0.12.1",
+      revision: "79d76aa6d40b1ae857f3606353bbb63186ff2085"
   license "Apache-2.0"
   head "https://github.com/k0sproject/k0sctl.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/k0sproject/homebrew-tap/releases/download/k0sctl-0.12.0"
-    sha256 cellar: :any_skip_relocation, big_sur:      "98d9bc068d49b7c5ba53b43884aa566c12c4d0f6158dc73d0f41e6a835689478"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "63357eb93ba430789f00828200f1a5daab253e34b747ae12a093eb89db45710b"
-  end
 
   depends_on "go@1.17" => :build
 
