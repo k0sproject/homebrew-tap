52cf5046bf2724219a2245aec4308b4446d44308
Kimmo Lehto <kimmo.lehto@gmail.com>
2022-01-05 11:47:51 +0000

    k0sctl: add 0.12.1 bottle.

--- Formula/k0sctl.rb
+++ Formula/k0sctl.rb
@@ -7,6 +7,12 @@ class K0sctl < Formula
   license "Apache-2.0"
   head "https://github.com/k0sproject/k0sctl.git", branch: "main"
 
+  bottle do
+    root_url "https://github.com/k0sproject/homebrew-tap/releases/download/k0sctl-0.12.1"
+    sha256 cellar: :any_skip_relocation, big_sur:      "ae2ea945ed73dca9bfec628024852ae73e37c050a6c8e7fb3eb3ff687ed9ceca"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "0e4a3d35d1004679627d6c50481e5e37bde87d36f61e38e3ad09ffe170f4d2bb"
+  end
+
   depends_on "go@1.17" => :build
 
   def install
