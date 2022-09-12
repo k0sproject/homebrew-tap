3106742a3292e559b9ec4eb2a90a5eba9da32d01
Kimmo Lehto <klehto@mirantis.com>
2022-09-12 15:20:25 +0300

    k0sctl 0.14.0
    K0sctl 0.14.0
    
    Use new completion dsl
    
    Split line
    
    How about like this?
    
    Shell names as strings
    
    Closes #14.
    
    Signed-off-by: Kimmo Lehto <kimmo.lehto@gmail.com>

--- Formula/k0sctl.rb
+++ Formula/k0sctl.rb
@@ -2,16 +2,10 @@ class K0sctl < Formula
   desc "Bootstrapping and management tool for k0s kubernetes clusters"
   homepage "https://github.com/k0sproject/k0sctl"
   url "https://github.com/k0sproject/k0sctl.git",
-      tag:      "v0.13.2",
-      revision: "71160254031c64fb42d138e138e4d26f145575f0"
+      tag:      "v0.14.0",
+      revision: "0cbfc474fbaa7f1e1e5e88de5cb5a35ef360170e"
   license "Apache-2.0"
   head "https://github.com/k0sproject/k0sctl.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/k0sproject/homebrew-tap/releases/download/k0sctl-0.13.2"
-    sha256 cellar: :any_skip_relocation, big_sur:      "6e19f3b63330446bc7c54189a76a87c36c958002a922f846e1e73338f8430d0a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "6d374710fe31b22ed182be809e96ad2dcc9912fe906f02b1f1d983c018e4325d"
-  end
 
   depends_on "go@1.19" => :build
 
@@ -20,14 +14,11 @@ class K0sctl < Formula
     bin.install "k0sctl"
     prefix.install_metafiles
 
-    output = Utils.safe_popen_read(bin/"k0sctl", "completion", "-s", "bash")
-    (bash_completion/"k0sctl").write output
-
-    output = Utils.safe_popen_read(bin/"k0sctl", "completion", "-s", "zsh")
-    (zsh_completion/"_k0sctl").write output
-
-    output = Utils.safe_popen_read(bin/"k0sctl", "completion", "-s", "fish")
-    (fish_completion/"k0sctl.fish").write output
+    generate_completions_from_executable(
+      bin/"k0sctl", "completion",
+      ["bash", "zsh", "fish"],
+      shell_parameter_format: :arg
+    )
   end
 
   test do
