ae1da4eec038f7a0522388b991526af4dbf6e293
Kimmo Lehto <klehto@mirantis.com>
2023-06-06 15:32:37 +0300

    k0sctl v0.15.2
    Closes #19.
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    Signed-off-by: Kimmo Lehto <kimmo.lehto@gmail.com>

--- Formula/k0sctl.rb
+++ Formula/k0sctl.rb
@@ -2,16 +2,10 @@ class K0sctl < Formula
   desc "Bootstrapping and management tool for k0s kubernetes clusters"
   homepage "https://github.com/k0sproject/k0sctl"
   url "https://github.com/k0sproject/k0sctl.git",
-      tag:      "v0.15.1",
-      revision: "102ec9928d6d3ac20cb45508181234720499b1cd"
+      tag:      "v0.15.2",
+      revision: "5300b1cf907e6b649807d53e47f4c6fd902fc740"
   license "Apache-2.0"
   head "https://github.com/k0sproject/k0sctl.git", branch: "main"
-
-  bottle do
-    root_url "https://github.com/k0sproject/homebrew-tap/releases/download/k0sctl-0.15.1"
-    sha256 cellar: :any_skip_relocation, ventura:      "20176134c462fc3f172dd421a7abad1b06a34eabc7cca698d602c184e712f16a"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "00d6e135d2eecc799afb0942fab7566264e999e0f569f8b155b2e44387fdbccb"
-  end
 
   depends_on "go" => :build
 
