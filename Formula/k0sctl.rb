5f1206b3380a6bb92be39e54cec24701ec335d76
Kimmo Lehto <klehto@mirantis.com>
2024-10-25 13:55:30 +0300

    k0sctl v0.19.2
    Closes #45.
    
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>
    Signed-off-by: Kimmo Lehto <kimmo.lehto@gmail.com>

--- Formula/k0sctl.rb
+++ Formula/k0sctl.rb
@@ -2,13 +2,13 @@ class K0sctl < Formula
   desc "Bootstrapping and management tool for k0s kubernetes clusters"
   homepage "https://github.com/k0sproject/k0sctl"
   url "https://github.com/k0sproject/k0sctl.git",
-      tag:      "v0.19.1",
-      revision: "06468474fdd8928666ea1faa2d5c32ac13e25103"
+      tag:      "v0.19.2",
+      revision: "081dfeb085418df4824e00778b092e77f84715ad"
   license "Apache-2.0"
   head "https://github.com/k0sproject/k0sctl.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/k0sproject/homebrew-tap/releases/download/k0sctl-0.19.1"
+    root_url "https://github.com/k0sproject/homebrew-tap/releases/download/k0sctl-0.19.2"
     sha256 cellar: :any_skip_relocation, arm64_sonoma: "841914daa01d974a50180c2cff241e48d3f85de5f20af20f6448d3a44a13bb98"
     sha256 cellar: :any_skip_relocation, ventura:      "be6db9cdb6799cd0669ec0aa5c7b381179bb684a5e6e1a06853df40981ab0329"
     sha256 cellar: :any_skip_relocation, x86_64_linux: "5b93d0691d68abd9c45f830f85d4409aadda2fe97fa18164e08588611c5e0d5f"
