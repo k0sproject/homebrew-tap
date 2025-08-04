57c271bc9f4fd7bfcf20b0f5227ca9484318c173
Kimmo Lehto <klehto@mirantis.com>
2025-08-04 09:06:59 +0300

    bootloose v0.9.1
    Signed-off-by: Kimmo Lehto <klehto@mirantis.com>

--- Formula/bootloose.rb
+++ Formula/bootloose.rb
@@ -2,8 +2,8 @@ class Bootloose < Formula
   desc "Creates containers that look like virtual machines"
   homepage "https://github.com/k0sproject/bootloose"
   url "https://github.com/k0sproject/bootloose.git",
-      tag:      "v0.9.0",
-      revision: "124c22f5c2d32e26d91995579ca184876d5a9195"
+      tag:      "v0.9.1",
+      revision: "2881820a231a6517e2a7b0c21ccaadf572c40b96"
   license "Apache-2.0"
   head "https://github.com/k0sproject/bootloose.git", branch: "main"
 
