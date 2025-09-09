59f34fcec9de62aec27877f5aed7c6c17b48da3f
Kimmo Lehto <kimmo.lehto@gmail.com>
2025-09-09 08:32:30 +0000

    bootloose: update 0.9.4 bottle.

--- Formula/bootloose.rb
+++ Formula/bootloose.rb
@@ -8,10 +8,10 @@ class Bootloose < Formula
   head "https://github.com/k0sproject/bootloose.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/k0sproject/homebrew-tap/releases/download/bootloose-0.9.1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "7d6f22258b41a8d591488e8bdda55ac8e3d64cdc16a4ed042ea9a66c08e02a2b"
-    sha256 cellar: :any_skip_relocation, ventura:      "f1a3bafa708ce87a12479562210faa78845d179b527b544fcea4f5fcd14d9afb"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "e8bb1527ae2a09b33016bb90045c4d5d8c57a08f30e04a57e568541f69c4e645"
+    root_url "https://github.com/k0sproject/homebrew-tap/releases/download/bootloose-0.9.4"
+    sha256 cellar: :any_skip_relocation, arm64_sonoma: "559a53b18409541fec89c56d4389415e2fc40c42d2d9fc4d13e7077ef145117e"
+    sha256 cellar: :any_skip_relocation, ventura:      "d5c382bc3f870350f6cee358df373d1e7ce384ce834da22220febbafa44dbad7"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "1ee7429025253068828f8c74d9768f5490c8644b129e7860b3ed053349ade39e"
   end
 
   depends_on "go" => :build
