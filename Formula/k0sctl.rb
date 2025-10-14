7a23615abc932d80440e4607514d628ff43a1df6
Kimmo Lehto <kimmo.lehto@gmail.com>
2025-10-14 14:03:51 +0000

    k0sctl: update 0.26.0 bottle.

--- Formula/k0sctl.rb
+++ Formula/k0sctl.rb
@@ -8,10 +8,9 @@ class K0sctl < Formula
   head "https://github.com/k0sproject/k0sctl.git", branch: "main"
 
   bottle do
-    root_url "https://github.com/k0sproject/homebrew-tap/releases/download/k0sctl-0.25.1"
-    sha256 cellar: :any_skip_relocation, arm64_sonoma: "166059f76d92ea5b16e7a3c78a58f11aecb6fbdfbea0bad244562e0832ae2121"
-    sha256 cellar: :any_skip_relocation, ventura:      "24e3a214285c47469b98ae309c0c397cb6c4b183bfad3be5f5ff28a2685b8567"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "8fcd9b5ff603433caa0c4bb23bc8b99f26c63a582729b532fccba16ac15f2af6"
+    root_url "https://github.com/k0sproject/homebrew-tap/releases/download/k0sctl-0.26.0"
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "4e1d3949eb1fd54c6dd2617d39e4a3588b1b83f3e7807e88c4f0e13439760261"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "b2149f8e5dcf346024d6a6e2baa95fbbe4e5e5214d2c4dd259bd651dc50c6bf3"
   end
 
   depends_on "go" => :build
